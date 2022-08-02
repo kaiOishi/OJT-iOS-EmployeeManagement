8bb4fe649bb249f7a45cb87733ae5fe82509253f
kai_oishi <>
2022-08-02 15:53:35 +0900

    EmployeeListViewController に rename

---
+++ EmployeeManagement/EmployeeListViewController.swift
@@ -0,0 +1,22 @@
+//
+//  EmployeeListViewController.swift
+//  EmployeeManagement
+//
+//  Created by kai_oishi on 2022/08/02.
+//
+
+import UIKit
+
+class EmployeeListViewController: UIViewController {
+
+    @IBOutlet weak var tabBar: UITabBar!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.title = "社員一覧"
+    }
+    
+    
+}
+
