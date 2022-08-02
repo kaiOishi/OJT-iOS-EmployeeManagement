9fadffa48b27689e8e307301f0d9a53f99bd40a9
kai_oishi <>
2022-08-02 16:41:12 +0900

    TabBarの文字サイズ変更

--- EmployeeManagement/EmployeeListViewController.swift
+++ EmployeeManagement/EmployeeListViewController.swift
@@ -11,17 +11,18 @@ class EmployeeListViewController: UIViewController, UITabBarDelegate {
 
     @IBOutlet weak var tabBar: UITabBar!
     
-    private let employeeList = UITabBarItem(tabBarSystemItem: .contacts, tag: 1)
-    private let addNewEmployee = UITabBarItem(tabBarSystemItem: .downloads, tag: 2)
+    private let employeeList = UITabBarItem(title: "社員一覧", image: nil, tag: 1)
+    private let addNewEmployee = UITabBarItem(title: "社員登録", image: nil, tag: 2)
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         self.title = "社員一覧"
-
         tabBar.items = [employeeList, addNewEmployee]
         tabBar.delegate = self
         tabBar.selectedItem = employeeList
+        
+        UITabBarItem.appearance().setTitleTextAttributes([NSAttributedString.Key.font: UIFont(name: "Avenir-Medium", size: 16)!], for: .normal)
     }
 
     override func viewDidAppear(_ animated: Bool) {
--- EmployeeManagement/InsertEmployeeViewController.swift
+++ EmployeeManagement/InsertEmployeeViewController.swift
@@ -9,6 +9,6 @@ import UIKit
 
 class InsertEmployeeViewController : UIViewController {
     override func viewDidLoad() {
-        super.viewDidLoad()
+        super.viewDidLoad()	
     }
 }
