ca4079cefd133a2f04683bc2a428a8cf96fc51f1
kai_oishi <>
2022-08-02 16:26:35 +0900

    社員登録画面のVCとStoryBoard追加

---
+++ EmployeeManagement/InsertEmployeeViewController.swift
@@ -0,0 +1,14 @@
+//
+//  InsertEmployeeViewController.swift
+//  EmployeeManagement
+//
+//  Created by kai_oishi on 2022/08/02.
+//
+
+import UIKit
+
+class InsertEmployeeViewController : UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
