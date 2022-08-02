3e44c8bc54a190aefa7dd41a73bfcd6d59bbd9f6
kai_oishi <>
2022-08-02 16:27:02 +0900

    画面遷移まで実装

--- EmployeeManagement/EmployeeListViewController.swift
+++ EmployeeManagement/EmployeeListViewController.swift
@@ -7,16 +7,33 @@
 
 import UIKit
 
-class EmployeeListViewController: UIViewController {
+class EmployeeListViewController: UIViewController, UITabBarDelegate {
 
     @IBOutlet weak var tabBar: UITabBar!
+    
+    private let employeeList = UITabBarItem(tabBarSystemItem: .contacts, tag: 1)
+    private let addNewEmployee = UITabBarItem(tabBarSystemItem: .downloads, tag: 2)
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         self.title = "社員一覧"
+
+        tabBar.items = [employeeList, addNewEmployee]
+        tabBar.delegate = self
+        tabBar.selectedItem = employeeList
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        tabBar.selectedItem = employeeList
     }
     
-    
+    func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
+        if item.tag == 2 {
+            let storyBoard = UIStoryboard(name: "InsertEmployeeViewController", bundle: nil)
+            let nextVC = storyBoard.instantiateViewController(withIdentifier: "InsertEmployeeViewController")
+            navigationController?.pushViewController(nextVC, animated: true)
+        }
+    }
 }
 
