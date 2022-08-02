aa9718faac18932063bc8a0f054f29206233fac5
kai_oishi <>
2022-08-02 15:22:16 +0900

    first commit

---
+++ EmployeeManagement/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EmployeeManagement
+//
+//  Created by kai_oishi on 2022/08/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.title = "hello world"
+    }
+
+
+}
+
